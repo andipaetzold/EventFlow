ba36e148d021bb05cad31e05d9323658b931b32f
Andreas Pätzold <andi.paetzold@gmail.com>
2015-02-13 01:06:55 +0100

    created environment

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt)
+{
+    var files = {
+        css: {
+            src:    "src/css/style.scss",
+            dest:   "src/css/style.min.css"
+        },
+        js: {
+            src:    "src/js/eventflow.js",
+            dest:   "src/js/eventflow.min.js"
+        }
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        /* CSS */
+        sass: {
+            options: {
+                sourcemap: "none",
+                noCache: true
+            },
+            dist: {
+                src:    files.css.src,
+                dest:   files.css.dest
+            }
+        },
+        autoprefixer: {
+            dist: {
+                src:    files.css.dest,
+                dest:   files.css.dest
+            }
+        },
+        cssmin: {
+            dist: {
+                src:    files.css.dest,
+                dest:   files.css.dest
+            }
+        },
+
+        /* JavaScript */
+        uglify: {
+            dist: {
+                src:    files.js.src,
+                dest:   files.js.dest
+            }
+        },
+
+        copy: {
+            dist: {
+                src:    files.js.src,
+                dest:   files.js.dest
+            }
+        },
+
+        /* Watch */
+        watch: {
+            dist: {
+                files: [files.css.src, files.js.src],
+                tasks: "dev"
+            }
+        },
+
+        /* Clean */
+        clean: {
+            dist: [
+                files.css.dest,
+                files.js.dest
+            ]
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-autoprefixer");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask("dev", ["clean", "sass", "autoprefixer", "copy"]);
+    grunt.registerTask("prod", ["dev", "cssmin", "uglify"]);
+    grunt.registerTask("default", "watch");
+};
