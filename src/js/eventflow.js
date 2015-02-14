54deb4533961d6cfe92aac3d6242a987a42d82e6
Andreas Pätzold <andi.paetzold@gmail.com>
2015-02-14 01:59:04 +0100

    added feed module for a better structure

--- src/js/eventflow.js
+++ src/js/eventflow.js
@@ -1,8 +1,90 @@
+// config
 var UPLOADCARE_PUBLIC_KEY = "8b5b4f0679e1d28fcb7e";
+var ref = new Firebase("https://scorching-fire-2873.firebaseio.com");
 
+// feed
+var feed = (function($, uc, undefined)
+{
+    var options = {
+        feedRef: null,
+        feedContainer: null
+    };
+
+    // config
+    var config = function(o)
+    {
+        $.extend(options, o);
+
+        options.feedRef.on("child_added", childAdded);
+    };
+
+    // push
+    var push = function(data, type)
+    {
+        if (options.feedRef != null)
+        {
+            options.feedRef.push({data: data, type: type, timestamp: Firebase.ServerValue.TIMESTAMP});
+        }
+    };
+
+    // push - text
+    var pushText = function(text)
+    {
+        if (text.length > 0)
+        {
+            push(text, "text");
+        }
+    };
+
+    // push - image
+    var pushImage = function(file)
+    {
+        var file = uc.fileFrom("object", file);
+        file.done(function(file)
+        {
+            if (file.isImage)
+            {
+                push(file.originalUrl, "image");
+            }
+        }).progress(function(upload)
+        {
+            console.log(upload.progress);
+        });
+    };
+
+    // update feed
+    var childAdded = function(snapshot, prev)
+    {
+        id   = snapshot.key();
+        data = snapshot.val();
+
+        item = $("<div></div>");
+        switch(data.type)
+        {
+            case "image":
+                $("<img>", {alt: "image" + id, src: data.data}).appendTo(item);
+                break;
+            case "text":
+                $("<p></p>").text(data.data).appendTo(item);
+                break;
+        }
+
+        options.feedContainer.prepend(item);
+    };
+
+
+    // return
+    return {
+        config:     config,
+        pushText:   pushText,
+        pushImage:  pushImage
+    };
+})(jQuery, uploadcare);
+
+// on ready
 $(function()
 {
-    var ref = new Firebase("https://scorching-fire-2873.firebaseio.com");
+    feed.config({feedRef: ref.child("feed"), feedContainer: $("#feed")});
 
     // load options
     ref.child("options").on("value", function(options)
@@ -19,53 +101,23 @@ $(function()
         console.error(e.code);
     });
 
-    // load feed
-    ref.child("feed").on("child_added", function(snapshot, prev)
-    {
-        id   = snapshot.key();
-        data = snapshot.val();
-
-        item = $("<div></div>");
-        switch(data.type)
-        {
-            case "image":
-                $("<img>", {alt: "image" + id, src: data.data}).appendTo(item);
-                break;
-            case "text":
-                $("<p></p>").text(data.data).appendTo(item);
-                break;
-        }
-
-        $("div#feed").prepend(item);
-    });
-
     // post event - text
     $("#post-text form").submit(function()
     {
         var textBox = $("input[type=text]", this);
-        ref.child("feed").push({data: textBox.val(), type: "text", timestamp: Firebase.ServerValue.TIMESTAMP});
+        feed.pushText(textBox.val());
         textBox.val("");
 
         return false;
     });
 
     // post event - image
-
     $("#post-image input[type=file]").change(function()
     {
         if(this.files.length == 1)
         {
-            var file = uploadcare.fileFrom("object", this.files[0]);
-            file.done(function(file)
-            {
-                if (file.isImage)
-                {
-                    ref.child("feed").push({data: file.originalUrl, type: "image", timestamp: Firebase.ServerValue.TIMESTAMP});
-                }
-            }).progress(function(upload)
-            {
-                console.log(upload.progress);
-            });
+            feed.pushImage(this.files[0]);
         }
+        $(this).replaceWith($(this).val("").clone(true));
     });
 });
